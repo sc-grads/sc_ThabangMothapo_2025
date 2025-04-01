d14e947dab921ddc2054c04d455885b757360d5b
Thabang-mothapo <thabang.mothapo@sambeconsulting.com>
2025-04-01 15:50:41 +0200

    Update Linux_command.sh

--- Linux/Linux_command.sh
+++ Linux/Linux_command.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+echo ""Hello World"
 
+-l #used for list
+-o #used for online
+
+/ #Root directory (everything starts here).
+/home #User home directories.
+/etc #Configuration files.
+/var #Logs and variable data.
+/bin #Essential system binaries and other executable programs.
+/usr #User-related programs.
+/tmp #Temporary files.
+pwd #Print working directory.
+ls #List files and directories.
+cd #Change directory.
+mkdir #Create a directory.
+rm #Remove files or directories.
+/media #Used to mount removable media.
+/srv	#Service data.
+/srv/www #Web server files.
+/run	#Runtime process data.
+/lost+found	#File recovery.
+/Export #Shared files systems.
+/root #The home directory for the root account.
+/opt #Optional or third party software.
