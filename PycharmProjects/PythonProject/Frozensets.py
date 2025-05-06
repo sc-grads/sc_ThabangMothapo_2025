3112aa9213854efff9e22a5a6512eef506192726
Thabang-mothapo <thabang.mothapo@sambeconsulting.com>
2025-05-06 10:36:58 +0200

    Delete PycharmProjects/PythonProject directory

--- PycharmProjects/PythonProject/Frozensets.py
+++
@@ -1,5 +0,0 @@
-things: frozenset = frozenset({1, 1, 2, 3, 3})
-print(things)
-print(frozenset)
-
-
--- PycharmProjects/PythonProject/SimpleAdder.py
+++
@@ -1,8 +0,0 @@
-print('Welcome to your simple adder!')
-print('------------------------------')
-
-a: str = input('Enter a value for "a":')
-b:str = input('Enter a value for "b":')
-print('-------------------------------')
-
-print('The result is:', int(a) + int(b))
